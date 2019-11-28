2cd7ec13c4852a2479fd1086ec4e4862ac0c27de
Paul Kim <pkayokay@gmail.com>
2019-11-27 23:50:40 -0500

    Add tailwind notes + config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'custom-color': 'white',
+      },
+      screens: {
+        range: { 'min': '200px', 'max': '500px'},
+        skip: [
+          { 'min': '200px', 'max': '500px'},
+          { 'min': '900px'}
+        ]
+      },
+    }
   },
-  variants: {},
-  plugins: []
+  variants: {
+    // Enabled by default
+    // backgroundColor: ['responsive', 'hover', 'focus'],
+  },
+  plugins: [],
 }
+
+// tw notes
+// - custom utility classes
+// - responsive classes
+// - hover, focus states
+// - variants - not out of the boxed, must be enabled on the modules property (hover, focus, group hover, responsive)
+// - We can define our own varients in our css file @varients hover { }
+// - options to override all custom classes with tailwind with important
