2de02f846636b710cc202b22051308d21043b96b
Paul Kim <pkayokay@gmail.com>
2019-12-07 16:54:30 -0500

    Add purgecss + enable only in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+let environment = {
   plugins: [
     require('autoprefixer'),
     require('postcss-import'),
@@ -9,6 +9,23 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
   ]
 }
+
+// Only run PurgeCSS in production (you can also add staging here)
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+
+
+module.exports = environment
