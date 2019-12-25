99c35441c9c1f9a316e20c321d415e5e11760602
Paul Kim <pkayokay@gmail.com>
2019-12-25 15:59:09 -0500

    Generate user model with devise

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  enum user_role: {
+    basic: 0,
+    pro: 1,
+  }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+end
